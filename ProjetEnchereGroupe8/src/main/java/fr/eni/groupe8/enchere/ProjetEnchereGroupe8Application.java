cf6b736aeebbd0b610495da092d000b1aea3595f
jdovale2023 <jdovale2023@12SE22-BBMRZS2.ad.campus-eni.fr>
2023-06-26 10:22:41 +0200

    Initialisation du projet

---
+++ ProjetEnchereGroupe8/src/main/java/fr/eni/groupe8/enchere/ProjetEnchereGroupe8Application.java
@@ -0,0 +1,13 @@
+package fr.eni.groupe8.enchere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEnchereGroupe8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetEnchereGroupe8Application.class, args);
+	}
+
+}
