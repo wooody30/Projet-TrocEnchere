6fc7b7c3a0aba2285d93a38a4275ee714266abe6
jdovale2023 <jdovale2023@12SE22-BBMRZS2.ad.campus-eni.fr>
2023-07-04 15:12:38 +0200

    idem

--- ProjetEnchereGroupe8/src/main/java/fr/eni/groupe8/enchere/bll/EncheresServiceImpl.java
+++ ProjetEnchereGroupe8/src/main/java/fr/eni/groupe8/enchere/bll/EncheresServiceImpl.java
@@ -11,35 +11,34 @@ import fr.eni.groupe8.enchere.bo.Enchere;
 import fr.eni.groupe8.enchere.bo.Utilisateur;
 import fr.eni.groupe8.enchere.dal.UtilisateurDAOimpl;
 
-@Service ("encheresService")
+@Service("encheresService")
 public class EncheresServiceImpl implements EncheresService {
 
-	/*private Utilisateur Utilisateur;
-	private Article article;
-	private Enchere enchere;
-	
-	public EncheresServiceImpl(Utilisateur utilisateur, Article article) {
-		this.Utilisateur = utilisateur;
-		this.article = article;
-	}*/
-	
+	/*
+	 * private Utilisateur Utilisateur; private Article article; private Enchere
+	 * enchere;
+	 * 
+	 * public EncheresServiceImpl(Utilisateur utilisateur, Article article) {
+	 * this.Utilisateur = utilisateur; this.article = article; }
+	 */
+
 	@Override
 	public boolean enchereValideSi(BigDecimal montantEnchere, Article article, Utilisateur utilisateur) {
-		
-		//if enchere inferieur a montant return false
+
+		// if enchere inferieur a montant return false
 		if (montantEnchere.compareTo(article.getPrixVente()) <= 0) {
-	        return false;
-	    }
-		//if solde utilisateur inferieur a enchere return false
+			return false;
+		}
+		// if solde utilisateur inferieur a enchere return false
 		if (utilisateur.getCredit().compareTo(montantEnchere) < 0) {
-	        return false;
-	    }
-		//if  dateDebut < enchere < dateFin return false
-		
+			return false;
+		}
+		// if dateDebut < enchere < dateFin return false
+
 		return true;
-		
+
 	}
-	
+
 	/*
 	 * @Override public Article encherir() {
 	 * 
